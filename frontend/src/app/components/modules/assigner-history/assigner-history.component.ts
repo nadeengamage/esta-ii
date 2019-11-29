4632fd2dab643e4392a2aadd8759db5caf3b3feb
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-29 20:37:07 +0530

    Created assigner history module

---
+++ frontend/src/app/components/modules/assigner-history/assigner-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigner-history',
+  templateUrl: './assigner-history.component.html',
+  styleUrls: ['./assigner-history.component.scss']
+})
+export class AssignerHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
