0be2ae74485e783e53f50a4745428b974c2ca908
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-29 20:37:27 +0530

    created assigner module

---
+++ frontend/src/app/components/modules/assigner/assigner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigner',
+  templateUrl: './assigner.component.html',
+  styleUrls: ['./assigner.component.scss']
+})
+export class AssignerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
