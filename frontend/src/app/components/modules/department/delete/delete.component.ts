e6fdc0f5620d88ea1a3348f24a71dfcc2fd44ea5
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-29 21:03:24 +0530

    Created CRUD modules

---
+++ frontend/src/app/components/modules/department/delete/delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.scss']
+})
+export class DepartmentDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
