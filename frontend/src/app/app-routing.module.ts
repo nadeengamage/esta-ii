78e3913b1763c4e80bf9dffe168bf8a7dfb14d34
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-29 20:39:10 +0530

    created routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from "./components/modules/dashboard/dashboard.component";
+import { EmployeeComponent } from './components/modules/employee/employee.component';
+import { DepartmentComponent } from './components/modules/department/department.component';
+import { AssignerComponent } from './components/modules/assigner/assigner.component';
+import { AssignerHistoryComponent } from './components/modules/assigner-history/assigner-history.component';
 
 const routes: Routes = [
-   
+   {
+       path: '',
+       component: DashboardComponent
+   },
+   {
+        path: 'departments',
+        component: DepartmentComponent
+   },
+   {
+       path: 'employees',
+       component: EmployeeComponent
+   },
+   {
+       path: 'assigners',
+       component: AssignerComponent
+   },
+   {
+       path: 'assigner-history',
+       component: AssignerHistoryComponent
+   }
 ]
 
 @NgModule({
