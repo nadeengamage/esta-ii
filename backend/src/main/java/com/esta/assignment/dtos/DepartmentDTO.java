460e92373bd7c25813c4640d73ddf82d9c215934
Nadeen Gamage <nadeengamage@gmail.com>
2019-12-11 11:19:11 +0530

    Update DepartmentDTO.java

--- backend/src/main/java/com/esta/assignment/dtos/DepartmentDTO.java
+++ backend/src/main/java/com/esta/assignment/dtos/DepartmentDTO.java
@@ -5,7 +5,7 @@ import java.sql.Time;
 
 public class DepartmentDTO implements Serializable {
 
-    private Long id;
+    private String id;
 
     private String name;
 
@@ -15,11 +15,11 @@ public class DepartmentDTO implements Serializable {
 
     private Boolean status;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
